46059f8598541b68548e28e5f94060b283de9938
Daniel Han <daniel.han@epitech.eu>
2016-07-12 12:32:50 +0200

    c and java try 1

--- HelloTravisJava/src/HelloTravis.java
+++ HelloTravisJava/src/HelloTravis.java
@@ -2,8 +2,8 @@
 public class HelloTravis {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
 		System.out.println("Hello Travis");
+		HelloWorld hw = new HelloWorld();
 	}
 
 }
